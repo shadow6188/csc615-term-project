68c0738dd8bfb056962a9b874166cc7e2903d88c
Bhupendra Chaudhary <bchaudhary@mail.sfsu.edu>
2021-05-10 18:02:20 -0700

    removed echo

--- echo_sensor.h
+++ echo_sensor.h
@@ -1,8 +1,8 @@
-#include <wiringPi.h>
+//#include <wiringPi.h>
 
-#define TRIG 6      //GPIO 
-#define ECHO 5      //GPIO 
+//#define TRIG 6      //GPIO 
+//#define ECHO 5      //GPIO 
 
 
-void initECHO();
-double distance();
+//void initECHO();
+//double distance();
