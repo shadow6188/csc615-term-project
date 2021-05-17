180f425343554fd7aeefd7744411e8fca93460fb
Gerardo Ochoa <gerardo.ochoa1@gmail.com>
2021-05-17 13:46:34 -0700

    moved echo sensor stuff into its own file.

---
+++ echo_sensors.h
@@ -0,0 +1,16 @@
+//
+// Created by gerardo on 5/17/21.
+//
+
+#ifndef CSC615_GROUP_TERM_PROJECT_PRAMODKHATRI10_ECHO_SENSORS_H
+#define CSC615_GROUP_TERM_PROJECT_PRAMODKHATRI10_ECHO_SENSORS_H
+
+#define TRIG 5 //assigning TRIG to GPIO 5
+#define ECHO 6 //assigning Echo to GPIO  6
+
+#define soundVelocity 34000
+
+void init_echo_sensors();
+double distance();
+
+#endif //CSC615_GROUP_TERM_PROJECT_PRAMODKHATRI10_ECHO_SENSORS_H
